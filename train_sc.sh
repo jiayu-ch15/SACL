b65602c9625e5dfb650d88443510417211b6d90c
zoeyuchao <1625360790@qq.com>
2020-07-07 00:00:43 +0800

    1

--- train_sc.sh
+++ train_sc.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 env="StarCraft2"
 map="3m"
-algo="3m_huber"
+algo="3m_huber5"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, seed is ${seed_max}"
@@ -9,5 +9,5 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, seed is ${seed_max}"
 for seed in `seq ${seed_max}`
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=5 python train.py --env_name ${env} --algorithm_name ${algo} --map_name ${map} --seed ${seed} --n_rollout_threads 8 --num_mini_batch 16 --ppo_epoch 4 --episode_length 400 --lr 7e-4 --value_loss_coef 1 --num_env_steps 10000000 --data_chunk_length 10 --max-grad-norm 0.5 --use_clipped_value_loss --use_huber_loss --huber_delta 2
+    CUDA_VISIBLE_DEVICES=5 python train.py --env_name ${env} --algorithm_name ${algo} --map_name ${map} --seed ${seed} --n_rollout_threads 8 --num_mini_batch 16 --ppo_epoch 4 --episode_length 400 --lr 7e-4 --value_loss_coef 1 --num_env_steps 10000000 --data_chunk_length 10 --max-grad-norm 0.5 --use_clipped_value_loss --use_huber_loss --huber_delta 5
 done
