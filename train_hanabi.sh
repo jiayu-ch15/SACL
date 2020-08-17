cb5c74dcbd51f96c5602bec7dadfd8cbaf7a4778
zoeyuchao <1625360790@qq.com>
2020-08-17 17:56:21 +0800

    add de

--- train_hanabi.sh
+++ train_hanabi.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 env="Hanabi"
-hanabi="Hanabi-Full"
+hanabi="Hanabi-Very-Small"
 num_agents=2
-algo="full_2_128"
+algo="very_small_test"
 seed_max=1
 
 echo "env is ${env}, algo is ${algo}, seed is ${seed_max}"
@@ -10,6 +10,6 @@ echo "env is ${env}, algo is ${algo}, seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=4 python train_hanabi.py --env_name ${env} --algorithm_name ${algo} --hanabi_name ${hanabi} --num_agents ${num_agents} --seed ${seed} --n_rollout_threads 200 --num_mini_batch 1 --episode_length 80 --num_env_steps 10000000 --ppo_epoch 20 --use_clipped_value_loss --cuda --hidden_size 128
+    CUDA_VISIBLE_DEVICES=4 python train_hanabi.py --env_name ${env} --algorithm_name ${algo} --hanabi_name ${hanabi} --num_agents ${num_agents} --seed ${seed} --n_rollout_threads 200 --num_mini_batch 1 --episode_length 80 --num_env_steps 10000000 --ppo_epoch 15 --use_clipped_value_loss --cuda --hidden_size 128 --data_chunk_length 20
     echo "training is done!"
 done
--- train_sc.sh
+++ train_sc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 env="StarCraft2"
-map="2s_vs_1sc"
+map="2c_vs_64zg"
 #algo="win_2s3z_xavier_uniform_attn"
-algo="ok_2s_vs_1sc_attn1642"
+algo="ok_2c_vs_64zg_attn1644"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, seed is ${seed_max}"
@@ -10,6 +10,6 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=6 python train_sc.py --env_name ${env} --algorithm_name ${algo} --map_name ${map} --seed ${seed} --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 15 --use_clipped_value_loss --cuda --attn --attn_heads 2
+    CUDA_VISIBLE_DEVICES=6 python train_sc.py --env_name ${env} --algorithm_name ${algo} --map_name ${map} --seed ${seed} --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 15 --use_clipped_value_loss --cuda --attn --attn_heads 4
     echo "training is done!"
 done
