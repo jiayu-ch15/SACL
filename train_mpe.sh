8bd25c2689435008b6e981ef768c823968962a75
zoeyuchao <1625360790@qq.com>
2020-09-21 22:05:21 +0800

    123

--- train_mpe.sh
+++ train_mpe.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 env="MPE"
-scenario="simple_speaker_listener"
+scenario="simple_spread"
 num_landmarks=3
-num_agents=2
-algo="gru/simple_speaker_listener_parallel128_length25_lr5e-4"
+num_agents=3
+algo="mlp/simple_spread_parallel128_length25_lr1e-3"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, seed is ${seed_max}"
@@ -11,6 +11,6 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, seed is ${seed_ma
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=7 python train_mpe.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_rollout_threads 128 --num_mini_batch 1 --episode_length 25 --num_env_steps 10000000 --ppo_epoch 15 --lr 5e-4 --gain 0.01 --share_policy
+    CUDA_VISIBLE_DEVICES=7 python train_mpe.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_rollout_threads 128 --num_mini_batch 1 --episode_length 25 --num_env_steps 10000000 --ppo_epoch 15 --lr 1e-3 --gain 0.01 --recurrent_policy
     echo "training is done!"
 done
