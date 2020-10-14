aa430ac5a250a810c19813be9a0ff8959a806441
zoeyuchao <1625360790@qq.com>
2020-10-14 22:35:08 +0800

    update sweep

--- scripts/train_agar.sh
+++ scripts/train_agar.sh
@@ -4,11 +4,6 @@ algo="new_agar_rollout24_batch16_length600_kill0_coop1_hidden128_attn11284_15e6_
 #algo="test"
 seed_max=1
 
-echo "env is ${env}, algo is ${algo}, seed is ${seed_max}"
-
-for seed in `seq ${seed_max}`;
-do
-    echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=4 python train_agar.py --env_name ${env} --algorithm_name ${algo} --seed ${seed} --n_rollout_threads 24 --num_mini_batch 16 --num_agents 2 --episode_length 600 --num_env_steps 40000000 --hidden_size 128 --attn --attn_N 1 --attn_size 128 --attn_heads 4 --lr 2.5e-4 --cuda
-    echo "training is done!"
-done
+echo "env is ${env}, algo is ${algo}, max seed is ${seed_max}"
+CUDA_VISIBLE_DEVICES=4 python train/train_agar.py --env_name ${env} --algorithm_name ${algo} --seed ${seed_max} --n_rollout_threads 24 --num_mini_batch 16 --num_agents 2 --episode_length 600 --num_env_steps 40000000 --hidden_size 128 --attn --attn_N 1 --attn_size 128 --attn_heads 4 --lr 2.5e-4 --cuda
+echo "training is done!"
--- scripts/train_boxlocking.sh
+++ scripts/train_boxlocking.sh
@@ -9,5 +9,5 @@ algo="test"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario_name}, num_agents is ${num_agents}, algo is ${algo}, max seed is ${seed_max}"
-CUDA_VISIBLE_DEVICES=7 python train_hns_transfertask.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario_name} --task_type ${task_type} --num_agents ${num_agents} --num_boxes ${num_boxes} --seed ${seed_max} --n_rollout_threads 1 --num_mini_batch 1 --episode_length 240 --num_env_steps 100000000 --ppo_epoch 15 --gain 1 --attn
+CUDA_VISIBLE_DEVICES=7 python train/train_hns_transfertask.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario_name} --task_type ${task_type} --num_agents ${num_agents} --num_boxes ${num_boxes} --seed ${seed_max} --n_rollout_threads 1 --num_mini_batch 1 --episode_length 240 --num_env_steps 100000000 --ppo_epoch 15 --gain 1 --attn
 echo "training is done!"
