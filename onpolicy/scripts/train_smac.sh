1b776451bb9052dd73746e93e2a33a59b2dba037
zoeyuchao <1625360790@qq.com>
2020-12-20 16:41:23 +0800

    fix a * bug in buffer

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -9,6 +9,6 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is 
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=5 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 2 --n_training_threads 1 --n_rollout_threads 2 --num_mini_batch 1 --episode_length 40 --num_env_steps 10000000 --ppo_epoch 5 --use_value_active_masks --use_eval --use_state_agent --user_name "zoeyuchao" --use_wandb
+    CUDA_VISIBLE_DEVICES=5 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 2 --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 --use_value_active_masks --use_eval --use_state_agent --user_name "zoeyuchao" --use_wandb
     echo "training is done!"
 done
