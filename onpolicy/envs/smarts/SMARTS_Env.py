0465525a513e6670dffd848c110f93a1c850312c
jz <253921509@qq.com>
2020-12-21 11:33:17 +0800

    add the rew_adapter.py and obs_adapter.py

--- onpolicy/envs/smarts/SMARTS_Env.py
+++ onpolicy/envs/smarts/SMARTS_Env.py
@@ -15,28 +15,25 @@ from smarts.core.utils.visdom_client import VisdomClient
 from envision.client import Client as Envision
 import math
 from scipy.spatial import distance
+from onpolicy.envs.smarts.obs_adapter_fc import \
+    get_lan_ttc_observation_adapter
+from onpolicy.envs.smarts.rew_adapter_fc import get_reward_adapter
 
 
-@dataclass
-class Adapter:
-    space: gym.Space
-    transform: Callable
-
-
-NEIGHBOR_NUM=3
-
-_LANE_TTC_OBSERVATION_SPACE = gym.spaces.Dict(
-    {
-        "distance_to_center": gym.spaces.Box(low=-1e10, high=1e10, shape=(1,)),
-        "angle_error": gym.spaces.Box(low=-np.pi, high=np.pi, shape=(1,)),
-        "speed": gym.spaces.Box(low=-1e10, high=1e10, shape=(1,)),
-        "steering": gym.spaces.Box(low=-1e10, high=1e10, shape=(1,)),
-        "ego_lane_dist": gym.spaces.Box(low=-1e10, high=1e10, shape=(3,)),
-        "ego_ttc": gym.spaces.Box(low=-1e10, high=1e10, shape=(3,)),
-        "neighbor":gym.spaces.Box(low=-1e3, high=1e3, shape=(NEIGHBOR_NUM * 5,)),
-        "proximity":gym.spaces.Box(low=-1e10, high=1e10, shape=(8,)),
-    }
-)
+def defin_obs_space_dict(neighbor_num):
+    _LANE_TTC_OBSERVATION_SPACE = gym.spaces.Dict(
+        {
+            "distance_to_center": gym.spaces.Box(low=-1e10, high=1e10, shape=(1,)),
+            "angle_error": gym.spaces.Box(low=-np.pi, high=np.pi, shape=(1,)),
+            "speed": gym.spaces.Box(low=-1e10, high=1e10, shape=(1,)),
+            "steering": gym.spaces.Box(low=-1e10, high=1e10, shape=(1,)),
+            "ego_lane_dist": gym.spaces.Box(low=-1e10, high=1e10, shape=(3,)),
+            "ego_ttc": gym.spaces.Box(low=-1e10, high=1e10, shape=(3,)),
+            "neighbor":gym.spaces.Box(low=-1e3, high=1e3, shape=(neighbor_num * 5,)),
+            "proximity":gym.spaces.Box(low=-1e10, high=1e10, shape=(8,)),
+        }
+    )
+    return _LANE_TTC_OBSERVATION_SPACE
 
 def get_dict_dim(space_dict):
     dim = 0
@@ -45,261 +42,10 @@ def get_dict_dim(space_dict):
         dim += reduce(lambda x, y: x*y, space)
     return dim
 
-
-def _lane_ttc_observation_adapter(env_observation):
-
-    ego = env_observation.ego_vehicle_state
-    waypoint_paths = env_observation.waypoint_paths
-    wps = [path[0] for path in waypoint_paths]
-
-    # distance of vehicle from center of lane
-    closest_wp = min(wps, key=lambda wp: wp.dist_to(ego.position))
-    signed_dist_from_center = closest_wp.signed_lateral_error(ego.position)
-    lane_hwidth = closest_wp.lane_width * 0.5
-    norm_dist_from_center = signed_dist_from_center / lane_hwidth
-
-    ego_ttc, ego_lane_dist = _ego_ttc_lane_dist(env_observation, closest_wp.lane_index)
-
-    if env_observation.neighborhood_vehicle_states is not None:
-        neighbor=cal_neighbor(env_observation, NEIGHBOR_NUM)
-    else:
-        neighbor = [0] * (NEIGHBOR_NUM * 5)
-
-    if env_observation.occupancy_grid_map is not None:
-        proximity = cal_proximity(env_observation)
-    else:
-        proximity = [0] * 8
-
-
-    return {
-        "distance_to_center": np.array([norm_dist_from_center]),
-        "angle_error": np.array([closest_wp.relative_heading(ego.heading)]),
-        "speed": np.array([ego.speed]),
-        "steering": np.array([ego.steering]),
-        "ego_ttc": np.array(ego_ttc),
-        "ego_lane_dist": np.array(ego_lane_dist),
-        "neighbor": np.array(neighbor),
-        "proximity": np.array(proximity)
-    }
-
-
-lane_ttc_observation_adapter = Adapter(
-    space=_LANE_TTC_OBSERVATION_SPACE, transform=_lane_ttc_observation_adapter
-)
-
-def _ego_ttc_lane_dist(env_observation, ego_lane_index):
-    ttc_by_p, lane_dist_by_p = _ttc_by_path(env_observation)
-
-    return _ego_ttc_calc(ego_lane_index, ttc_by_p, lane_dist_by_p)
-
-
-def _cal_angle(vec):
-
-    if vec[1] < 0:
-        base_angle = math.pi
-        base_vec = np.array([-1.0, 0.0])
-    else:
-        base_angle = 0.0
-        base_vec = np.array([1.0, 0.0])
-
-    cos = vec.dot(base_vec) / np.sqrt(vec.dot(vec) + base_vec.dot(base_vec))
-    angle = math.acos(cos)
-    return angle + base_angle
-
-def _get_closest_vehicles(ego, neighbor_vehicles, n):
-    ego_pos = ego.position[:2]
-    groups = {i: (None, 1e10) for i in range(n)}
-    partition_size = math.pi * 2.0 / n
-    # get partition
-    for v in neighbor_vehicles:
-        v_pos = v.position[:2]
-        rel_pos_vec = np.asarray([v_pos[0] - ego_pos[0], v_pos[1] - ego_pos[1]])
-        # calculate its partitions
-        angle = _cal_angle(rel_pos_vec)
-        i = int(angle / partition_size)
-        dist = np.sqrt(rel_pos_vec.dot(rel_pos_vec))
-        if dist < groups[i][1]:
-            groups[i] = (v, dist)
-
-    return groups
-
-def cal_neighbor(env_obs, closest_neighbor_num):
-    ego = env_obs.ego_vehicle_state
-    neighbor_vehicle_states = env_obs.neighborhood_vehicle_states
-    # dist, speed, ttc, pos
-    features = np.zeros((closest_neighbor_num, 5))
-    # fill neighbor vehicles into closest_neighboor_num areas
-    surrounding_vehicles = _get_closest_vehicles(
-        ego, neighbor_vehicle_states, n=closest_neighbor_num
-    )
-
-    heading_angle = ego.heading + math.pi / 2.0
-    ego_heading_vec = np.asarray([math.cos(heading_angle), math.sin(heading_angle)])
-    for i, v in surrounding_vehicles.items():
-        if v[0] is None:
-            continue
-        v = v[0]
-        rel_pos = np.asarray(
-            list(map(lambda x: x[0] - x[1], zip(v.position[:2], ego.position[:2])))
-        )
-
-        rel_dist = np.sqrt(rel_pos.dot(rel_pos))
-
-        v_heading_angle = math.radians(v.heading)
-        v_heading_vec = np.asarray(
-            [math.cos(v_heading_angle), math.sin(v_heading_angle)]
-        )
-
-        ego_heading_norm_2 = ego_heading_vec.dot(ego_heading_vec)
-        rel_pos_norm_2 = rel_pos.dot(rel_pos)
-        v_heading_norm_2 = v_heading_vec.dot(v_heading_vec)
-
-        ego_cosin = ego_heading_vec.dot(rel_pos) / np.sqrt(
-            ego_heading_norm_2 + rel_pos_norm_2
-        )
-
-        v_cosin = v_heading_vec.dot(rel_pos) / np.sqrt(
-            v_heading_norm_2 + rel_pos_norm_2
-        )
-
-        rel_speed = 0
-        if ego_cosin <= 0 and v_cosin > 0:
-            rel_speed = 0
-        else:
-            rel_speed = ego.speed * ego_cosin - v.speed * v_cosin
-
-        ttc = min(rel_dist / max(1e-5, rel_speed), 1e3)
-
-        features[i, :] = np.asarray(
-            [rel_dist, rel_speed, ttc, rel_pos[0], rel_pos[1]]
-        )
-
-    return features.reshape((-1,))
-
-
-def _ttc_by_path(env_observation):
-    ego = env_observation.ego_vehicle_state
-    waypoint_paths = env_observation.waypoint_paths
-    neighborhood_vehicle_states = env_observation.neighborhood_vehicle_states
-
-    # first sum up the distance between waypoints along a path
-    # ie. [(wp1, path1, 0),
-    #      (wp2, path1, 0 + dist(wp1, wp2)),
-    #      (wp3, path1, 0 + dist(wp1, wp2) + dist(wp2, wp3))]
-
-    wps_with_lane_dist = []
-    for path_idx, path in enumerate(waypoint_paths):
-        lane_dist = 0.0
-        for w1, w2 in zip(path, path[1:]):
-            wps_with_lane_dist.append((w1, path_idx, lane_dist))
-            lane_dist += np.linalg.norm(w2.pos - w1.pos)
-        wps_with_lane_dist.append((path[-1], path_idx, lane_dist))
-
-    # next we compute the TTC along each of the paths
-    ttc_by_path_index = [1000] * len(waypoint_paths)
-    lane_dist_by_path_index = [1] * len(waypoint_paths)
-    if neighborhood_vehicle_states is not None:
-        for v in neighborhood_vehicle_states:
-            # find all waypoints that are on the same lane as this vehicle
-            wps_on_lane = [
-                (wp, path_idx, dist)
-                for wp, path_idx, dist in wps_with_lane_dist
-                if wp.lane_id == v.lane_id
-            ]
-
-            if not wps_on_lane:
-                # this vehicle is not on a nearby lane
-                continue
-
-            # find the closest waypoint on this lane to this vehicle
-            nearest_wp, path_idx, lane_dist = min(
-                wps_on_lane, key=lambda tup: np.linalg.norm(tup[0].pos - vec_2d(v.position))
-            )
-
-            if np.linalg.norm(nearest_wp.pos - vec_2d(v.position)) > 2:
-                # this vehicle is not close enough to the path, this can happen
-                # if the vehicle is behind the ego, or ahead past the end of
-                # the waypoints
-                continue
-
-            relative_speed_m_per_s = (ego.speed - v.speed) * 1000 / 3600
-            if abs(relative_speed_m_per_s) < 1e-5:
-                relative_speed_m_per_s = 1e-5
-
-            ttc = lane_dist / relative_speed_m_per_s
-            ttc /= 10
-            if ttc <= 0:
-                # discard collisions that would have happened in the past
-                continue
-
-            lane_dist /= 100
-            lane_dist_by_path_index[path_idx] = min(
-                lane_dist_by_path_index[path_idx], lane_dist
-            )
-            ttc_by_path_index[path_idx] = min(ttc_by_path_index[path_idx], ttc)
-
-    return ttc_by_path_index, lane_dist_by_path_index
-
-
-def _ego_ttc_calc(ego_lane_index, ttc_by_path, lane_dist_by_path):
-    ego_ttc = [0] * 3
-    ego_lane_dist = [0] * 3
-
-    ego_ttc[1] = ttc_by_path[ego_lane_index]
-    ego_lane_dist[1] = lane_dist_by_path[ego_lane_index]
-
-    max_lane_index = len(ttc_by_path) - 1
-    min_lane_index = 0
-    if ego_lane_index + 1 > max_lane_index:
-        ego_ttc[2] = 0
-        ego_lane_dist[2] = 0
-    else:
-        ego_ttc[2] = ttc_by_path[ego_lane_index + 1]
-        ego_lane_dist[2] = lane_dist_by_path[ego_lane_index + 1]
-    if ego_lane_index - 1 < min_lane_index:
-        ego_ttc[0] = 0
-        ego_lane_dist[0] = 0
-    else:
-        ego_ttc[0] = ttc_by_path[ego_lane_index - 1]
-        ego_lane_dist[0] = lane_dist_by_path[ego_lane_index - 1]
-    return ego_ttc, ego_lane_dist
-
-
-def proximity_detection(OGM):
-    """
-    Detects other vehicles in the vicinity of the ego vehicle
-    hard coded for OGM(64, 64, 0.25)
-    """
-    boxes = []
-    boxes += [
-        OGM[11:25, 23:27],  # front left
-        OGM[11:25, 27:37],  # front center
-        OGM[11:25, 37:41],  # front right
-        OGM[25:39, 23:27],  # left
-        OGM[25:39, 37:41],  # right
-        OGM[41:53, 23:27],  # back left
-        OGM[41:53, 27:37],  # back center
-        OGM[41:53, 37:41],  # back right
-    ]
-    output = np.array([b.max() > 0 for b in boxes], np.float32)
-    return output
-
-def cal_proximity(env_obs):
-    proximity = proximity_detection(env_obs.occupancy_grid_map[1])
-    return proximity
-
-def get_distance_from_center(env_obs):
-    ego_state = env_obs.ego_vehicle_state
-    wp_paths = env_obs.waypoint_paths
-    closest_wps = [path[0] for path in wp_paths]
-
-    # distance of vehicle from center of lane
-    closest_wp = min(closest_wps, key=lambda wp: wp.dist_to(ego_state.position))
-    signed_dist_from_center = closest_wp.signed_lateral_error(ego_state.position)
-    lane_hwidth = closest_wp.lane_width * 0.5
-    norm_dist_from_center = signed_dist_from_center / lane_hwidth
-
-    return norm_dist_from_center
+@dataclass
+class Adapter:
+    space: gym.Space
+    transform: Callable
 
 
 def action_adapter(policy_action):
@@ -310,147 +56,6 @@ def action_adapter(policy_action):
     action_dict = ["keep_lane", "slow_down", "change_lane_left", "change_lane_right"]
     return action_dict[action]
 
-def observation_adapter(env_observation):
-    obs = lane_ttc_observation_adapter.transform(env_observation)
-    obs_flatten = np.concatenate(list(obs.values()), axis=0)
-    return obs_flatten
-
-
-#def reward_adapter(env_obs, env_reward):
-#    return env_reward
-
-def get_reward_adapter(adapter_type="vanilla"):
-    def vanilla(env_obs, env_reward):
-        return env_reward
-
-    def single_frame(last_env_obs, env_obs, env_reward):
-        penalty, bonus = 0.0, 0.0
-        obs = _lane_ttc_observation_adapter(env_obs)
-        last_obs = _lane_ttc_observation_adapter(last_env_obs)
-
-        neighbor_features = obs.get("neighbor", None)
-        last_neighbor_feature = last_obs.get("neighbor", None)
-
-        # dealing with neighbor_features
-        if neighbor_features is not None:
-            new_neighbor_feature = neighbor_features.reshape((-1, 5))
-            last_neighbor_feature = last_neighbor_feature.reshape((-1, 5))
-            mean_dist = np.mean(new_neighbor_feature[:, 0])
-            mean_ttc = np.mean(new_neighbor_feature[:, 2])
-            mean_dist2 = np.mean(last_neighbor_feature[:, 0])
-            # mean_speed2 = np.mean(last_neighbor_feature[:, 1])
-            mean_ttc2 = np.mean(last_neighbor_feature[:, 2])
-
-            # this penalty should considering the speed
-            # if speed is ...
-            ego_speed = env_obs.ego_vehicle_state.speed
-            penalty += np.tanh(ego_speed) * (
-                0.03 * (mean_dist - mean_dist2)
-                # - 0.01 * (mean_speed - mean_speed2)
-                + 0.01 * (mean_ttc - mean_ttc2)
-            )
-
-        # ======== Penalty: distance to goal =========
-        goal = env_obs.ego_vehicle_state.mission.goal
-
-        last_ego_2d_pos = last_env_obs.ego_vehicle_state.position[:2]
-        ego_2d_pos = env_obs.ego_vehicle_state.position[:2]
-
-        if hasattr(goal, "position"):
-            goal_pos = goal.position
-            last_goal_dist = distance.euclidean(last_ego_2d_pos, goal_pos)
-            goal_dist = distance.euclidean(ego_2d_pos, goal_pos)
-            penalty += 0.1 * (last_goal_dist - goal_dist)
-        else:
-            raise ValueError(f"Goal type: {type(goal)} has no attr named: position.")
-
-        # ======== Penalty: distance to the center
-        if last_obs.get("distance_to_center") is not None:
-            diff_dist_to_center_penalty = np.abs(
-                last_obs["distance_to_center"]
-            ) - np.abs(obs["distance_to_center"])
-            penalty += 0.01 * diff_dist_to_center_penalty[0]
-
-        # ======== Penalty & Bonus: event (collision, off_road, reached_goal, reached_max_episode_steps)
-        ego_events = env_obs.events
-        # ::collision
-        penalty += -50.0 if len(ego_events.collisions) > 0 else 0.0
-        # ::off road
-        penalty += -50.0 if ego_events.off_road else 0.0
-        # ::reach goal
-        if ego_events.reached_goal:
-            bonus += 20.0
-
-        # ::reached max_episode_step
-        if ego_events.reached_max_episode_steps:
-            penalty += -0.5
-        else:
-            bonus += 0.5
-
-        # ======== Penalty: penalise sharp turns done at high speeds =======
-        if env_obs.ego_vehicle_state.speed > 60:
-            steering_penalty = -pow(
-                (env_obs.ego_vehicle_state.speed - 60)
-                / 20
-                * env_obs.ego_vehicle_state.steering
-                / 4,
-                2,
-            )
-        else:
-            steering_penalty = 0
-        penalty += 0.1 * steering_penalty
-
-        # ========= Bonus: environment reward (distance travelled) ==========
-        bonus += 0.05 * env_reward
-        return bonus + penalty
-
-    def cruising(env_obs, env_reward):
-        global lane_crash_flag
-        global intersection_crash_flag
-
-        distance_from_center = get_distance_from_center(env_obs)
-
-        center_penalty = -np.abs(distance_from_center)
-
-        # penalise sharp turns done at high speeds
-        if env_obs.ego_vehicle_state.speed * 3.6 > 60:
-            steering_penalty = -pow(
-                (env_obs.ego_vehicle_state.speed * 3.6 - 60)
-                / 20
-                * (env_obs.ego_vehicle_state.steering)
-                * 45
-                / 4,
-                2,
-            )
-        else:
-            steering_penalty = 0
-
-        # penalise close proximity to lane cars
-        if lane_crash_flag:
-            crash_penalty = -5
-        else:
-            crash_penalty = 0
-
-        # penalise close proximity to intersection cars
-        if intersection_crash_flag:
-            crash_penalty -= 5
-
-        total_reward = np.sum([1.0 * env_reward])
-        total_penalty = np.sum(
-            [0.1 * center_penalty, 1 * steering_penalty, 1 * crash_penalty]
-        )
-
-        return (total_reward + total_penalty) / 200.0
-
-    return {
-        "vanilla": vanilla,
-        "single_frame": single_frame,
-        "cruising": cruising,
-    }[adapter_type]
-
-
-
-
 
 class SMARTSEnv(gym.Env):
     metadata = {"render.modes": ["human"]}
@@ -460,13 +65,12 @@ class SMARTSEnv(gym.Env):
         self._log = logging.getLogger(self.__class__.__name__)
         smarts.core.seed(seed)
         self._dones_registered = 0
-        global NEIGHBOR_NUM
-        NEIGHBOR_NUM = all_args.neighbor_num
-
+        self.obs_dict=defin_obs_space_dict(all_args.neighbor_num)
+        self.neighbor_num=all_args.neighbor_num
 
         self.rews_mode=all_args.rews_mode
         self.n_agents = all_args.num_agents
-        self.obs_dim = get_dict_dim(_LANE_TTC_OBSERVATION_SPACE)
+        self.obs_dim = get_dict_dim(self.obs_dict)
         self.act_dim = 4
         self.observation_space = [gym.spaces.Box(low=-1e10, high=1e10, shape=(self.obs_dim,))] * self.n_agents
         self.share_observation_space = [gym.spaces.Box(low=-1e10, high=1e10, shape=(self.obs_dim,))] * self.n_agents
@@ -475,13 +79,12 @@ class SMARTSEnv(gym.Env):
         self.agent_ids = ["Agent %i" % i for i in range(self.n_agents)]
         self.scenarios = [(all_args.scenario_path + all_args.scenario_name)]
 
-        self.headless = all_args.headless
         self.seed = seed
         self._agent_specs = {
             agent_id: AgentSpec(
                 interface=AgentInterface.from_type(AgentType.VulnerDis, max_episode_steps=all_args.episode_length),
-                observation_adapter=observation_adapter,
-                reward_adapter=get_reward_adapter(all_args.rews_mode),
+                observation_adapter=self.get_obs_adapter(),
+                reward_adapter=get_reward_adapter(all_args.rews_mode,self.neighbor_num),
                 action_adapter=action_adapter,
             )
             for agent_id in self.agent_ids
@@ -534,6 +137,16 @@ class SMARTSEnv(gym.Env):
             "scenario_routes": scenario.route or "",
             "mission_hash": str(hash(frozenset(scenario.missions.items()))),
         }
+
+    def get_obs_adapter(self):
+        def observation_adapter(env_observation):
+            lane_ttc_observation_adapter = Adapter(
+                space=self.obs_dict, transform=get_lan_ttc_observation_adapter(self.neighbor_num)
+            )
+            obs = lane_ttc_observation_adapter.transform(env_observation)
+            obs_flatten = np.concatenate(list(obs.values()), axis=0)
+            return obs_flatten
+        return observation_adapter
 
     def base_reset(self):
         scenario = next(self._scenarios_iterator)
@@ -594,7 +207,7 @@ class SMARTSEnv(gym.Env):
 
             if self.rews_mode=="vanilla":
                 rewards[agent_id] = agent_spec.reward_adapter(observation, reward)
-            elif self.rews_mode=="single_frame":
+            elif self.rews_mode=="standard":
 
                 rewards[agent_id] = agent_spec.reward_adapter(self.last_obs[agent_id], observation, reward)
             elif self.rews_mode == "cruising":
