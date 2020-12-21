894988b7cde7b9e92d183a870afa184ad25eaed6
jz <253921509@qq.com>
2020-12-21 14:46:23 +0800

    smarts

--- onpolicy/scripts/train/train_smarts.py
+++ onpolicy/scripts/train/train_smarts.py
@@ -53,7 +53,7 @@ def parse_args(args, parser):
     parser.add_argument('--born_lane_id', nargs='+', type=int, default=1, help="index of lane that agents locate at when borning")
     parser.add_argument('--target_position_offset', nargs='+', type=int, default=1, help="distance between agents' target position to the middle")
     parser.add_argument('--target_lane_id', nargs='+', type=int, default=1, help="index of lane that agents target at")
-    parser.add_argument("--rews_mode", type=str, default="single_frame", help="used to specify env's rew")
+    parser.add_argument("--rews_mode", type=str, default="standard", help="used to specify env's rew")
 
     parser.add_argument('--neighbor_num', type=int, default=3, help="number of neighbor you can see in the env")
 
