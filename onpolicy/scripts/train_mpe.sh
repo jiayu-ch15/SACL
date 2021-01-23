ffd23fdaae3ad7afa74bca345732ea9ed2d3e812
zoeyuchao <1625360790@qq.com>
2021-01-23 20:49:04 +0800

    fix bug

--- onpolicy/scripts/train_mpe.sh
+++ onpolicy/scripts/train_mpe.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 env="MPE"
-scenario="simple_spread"  # simple_speaker_listener # simple_spread
+scenario="simple_speaker_listener"  # simple_speaker_listener # simple_spread
 num_landmarks=3
-num_agents=3
+num_agents=2
 algo="rmappo"
-exp="ippo"
-seed_max=1
+exp="rnn_mini4"
+seed_max=10
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=3 python train/train_mpe.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed 8 --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 1 --episode_length 25 --num_env_steps 20000000 --ppo_epoch 15 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --use_centralized_V
+    CUDA_VISIBLE_DEVICES=2 python train/train_mpe.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 4 --episode_length 25 --num_env_steps 2000000 --ppo_epoch 15 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --share_policy
     echo "training is done!"
 done
