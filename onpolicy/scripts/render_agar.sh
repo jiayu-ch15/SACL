308a6b8740664eae50659a0605d1dabbdea56fae
EthanYang233 <yangyuxiang@live.com>
2021-02-16 18:32:25 +0800

    a

--- onpolicy/scripts/render_agar.sh
+++ onpolicy/scripts/render_agar.sh
@@ -12,5 +12,5 @@ seed_max=1
 echo "env is ${env}"
 for seed in `seq ${seed_max}`
 do
-    CUDA_VISIBLE_DEVICES=0 python render/render_agar.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 --use_render --episode_length 25 --render_episodes 5 --use_wandb False
+    CUDA_VISIBLE_DEVICES=0 python render/render_agar.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 --use_render --episode_length 25 --render_episodes 5 --use_wandb False
 done
