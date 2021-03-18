025641f53adc45db10361f4bee6fbc4b1dde8372
zoeyuchao <1625360790@qq.com>
2021-03-18 16:28:06 +0800

    update

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 env="StarCraft2"
 map="corridor"
-algo="mappo"
-exp="mlp"
+algo="rmappo"
+exp="lstm"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 50 --n_training_threads 127 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 --use_value_active_masks --use_eval --add_center_xy --user_name "zoeyuchao" --use_state_agent --use_recurrent_policy
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 1 --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 --use_value_active_masks --use_eval --user_name "zoeyuchao" --use_influence_policy 
 done
