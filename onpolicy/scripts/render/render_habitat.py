94945aa609f70280c6d03dd2386623bdc7b2fbac
zoeyuchao <1625360790@qq.com>
2021-03-26 19:01:31 +0800

    update

---
+++ onpolicy/scripts/render/render_habitat.py
@@ -0,0 +1,29 @@
+import os
+import imageio
+
+def get_folders(dir, folders):
+    get_dir = os.listdir(dir)
+    for i in get_dir:          
+        sub_dir = os.path.join(dir, i)
+        if os.path.isdir(sub_dir): 
+            folders.append(sub_dir) 
+            get_folders(sub_dir, folders)
+
+gif_dir = "/home/yuchao/project/onpolicy/onpolicy/scripts/results/Habitat/mappo/debug/run93/gifs"
+
+print("generating gifs....")
+folders = []
+get_folders(gif_dir, folders)
+filer_folders = [folder for folder in folders if "all" in folder or "merge" in folder]
+
+for folder in filer_folders:
+    image_names = os.listdir(folder)
+
+    frames = []
+    for image_name in image_names:
+        image_path = os.path.join(folder, image_name)
+        frame = imageio.imread(image_path)
+        frames.append(frame)
+
+    imageio.mimsave(str(folder) + '/render.gif', frames, duration=0.001)
+print("done!")
--- onpolicy/scripts/render_habitat.sh
+++ onpolicy/scripts/render_habitat.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 env="Habitat"
 scenario="pointnav_gibson"
-num_agents=1
+num_agents=2
 algo="mappo"
 exp="debug"
 seed_max=1
