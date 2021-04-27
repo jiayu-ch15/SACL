15b8da22806ee52c113f742bcc1ed27304e25bfc
huangruixin <18860020911@163.com>
2021-04-27 20:54:59 +0800

    update train_human.sh

--- onpolicy/scripts/train_human.sh
+++ onpolicy/scripts/train_human.sh
@@ -5,7 +5,7 @@ num_landmarks=2
 num_good_agents=1
 num_adversaries=3
 algo="rmappo"
-exp="fixed_four_dirrew_encoder_1prey_obsmask_alpha0.5_view2.0_allreach"
+exp="fixed_four_dirrew_encoder_1prey_obsmask_alpha0.005_view0.1"
 seed_max=1
 ulimit -n 234567
 
@@ -13,7 +13,7 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, ma
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=0 python train/train_human.py --direction_alpha 0.5 --view_threshold 2.0 --use_all_reach --add_direction_encoder --use_fixed_prey --use_pos_four_direction \
+    CUDA_VISIBLE_DEVICES=0 python train/train_human.py --direction_alpha 0.005 --view_threshold 0.1 --add_direction_encoder --use_fixed_prey --use_pos_four_direction \
     --use_direction_reward --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_good_agents ${num_good_agents} \
     --num_adversaries ${num_adversaries} --num_landmarks ${num_landmarks} --seed 1 --n_training_threads 1 --n_rollout_threads 128 --num_mini_batch 1 --episode_length 25 \
     --num_env_steps 50000000 --ppo_epoch 10 --gain 0.01 --lr 7e-4 --critic_lr 7e-4 --wandb_name "zoeyuchao" --user_name "zoeyuchao" --use_eval 
