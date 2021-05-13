fc2dc8d873f991aff07460a5502a72df17e2abd0
zoeyuchao <1625360790@qq.com>
2021-05-13 14:03:16 +0800

    update

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 env="StarCraft2"
-map="6h_vs_8z"
-algo="rmappo"
-exp="debug"
+map="corridor"
+algo="mappo"
+exp="concat_global_local_dead"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --use_wandb --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 1 --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 --episode_length 400 --num_env_steps 10000000 --ppo_epoch 15 --use_value_active_masks --use_eval --user_name "zoeyuchao" --use_global_local_state
+    CUDA_VISIBLE_DEVICES=0 python train/train_smac.py --env_name ${env} \
+    --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 2 \
+    --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 \
+    --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 \
+    --use_value_active_masks --use_eval --user_name "zoeyuchao" \
+    --use_global_local_state --use_state_agent --use_recurrent_policy
 done
