5f155403019e09c00a4b181f3466e503887f895d
zoeyuchao <1625360790@qq.com>
2021-05-15 12:02:21 +0800

    upda

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -13,5 +13,6 @@ do
     --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 1 \
     --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 1 \
     --episode_length 400 --num_env_steps 10000000 --ppo_epoch 15 \
-    --use_value_active_masks  --user_name "zoeyuchao" --use_wandb --use_global_local_state --use_state_agent --use_recurrent_policy --use_stacked_frames --stacked_frames 4 #--use_eval
+    --use_value_active_masks --use_eval --user_name "zoeyuchao" \
+    --use_global_local_state --use_state_agent --use_recurrent_policy --use_stacked_frames --stacked_frames 4
 done
