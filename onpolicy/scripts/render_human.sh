64a16dbb96f638b7cc6fd0e4b1c29a456570f118
huangruixin <18860020911@163.com>
2021-05-23 21:16:54 +0800

    update

--- onpolicy/scripts/render_human.sh
+++ onpolicy/scripts/render_human.sh
@@ -3,7 +3,7 @@ env="Human"
 scenario="simple_human"
 num_landmarks=2
 num_good_agents=1
-num_adversaries=3
+num_adversaries=1
 algo="rmappo"
 exp="render"
 seed_max=1
@@ -11,9 +11,11 @@ seed_max=1
 echo "env is ${env}"
 for seed in `seq ${seed_max}`
 do
-    CUDA_VISIBLE_DEVICES=0 python render/render_human.py --use_fixed_prey --use_pos_four_direction --save_gifs \
-    -env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_good_agents ${num_good_agents} \
-    --num_adversaries ${num_adversaries} --num_landmarks ${num_landmarks} --seed ${seed} \
+    CUDA_VISIBLE_DEVICES=0 python render/render_human.py \
+    --use_human_command --use_fixed_prey --use_pos_four_direction --use_direction_reward --add_direction_encoder --use_all_reach --use_noisy_command \
+    --direction_alpha 1 --view_threshold 0.5 --noisy_level 9 \
+    --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_good_agents ${num_good_agents} \
+    --num_adversaries ${num_adversaries} --num_landmarks ${num_landmarks} --seed ${seed} --save_gifs \
     --n_training_threads 1 --n_rollout_threads 1 --use_render --episode_length 25 --render_episodes 5 \
-    --model_dir "/home/yuchao/project/onpolicy/onpolicy/scripts/results/Human/simple_tag/rmappo/debug/wandb/run-20210413_174708-1zlup6n3/files/"
+    --model_dir "/home/huangruixin/render/MPE/fixed_four_dirrew_encoder_1predator_1prey_obsmask_alpha1_view0.5_NoisyLevel9"
 done
