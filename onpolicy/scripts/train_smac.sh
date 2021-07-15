3c095c9954b2f609d7cffbae8afb5ffb4c10ca69
zoeyuchao <1625360790@qq.com>
2021-07-15 12:09:07 +0800

    update

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 env="StarCraft2"
-map="corridor"
-algo="mappo"
-exp="mlp_clip0.1"
+map="3s5z_vs_3s6z"
+algo="rmappo"
+exp="clip0.3_ppo5"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
@@ -10,9 +10,9 @@ for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
     CUDA_VISIBLE_DEVICES=0 python train/train_smac.py --env_name ${env} \
-    --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 5 \
+    --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 2 \
     --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 1 \
     --episode_length 400 --num_env_steps 10000000 --ppo_epoch 5 \
     --use_value_active_masks --use_eval --user_name "zoeyuchao" \
-    --clip_param 0.1 --use_recurrent_policy
+    --clip_param 0.3
 done
