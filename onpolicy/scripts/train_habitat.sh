970b6bc6792a1cd21ba73fc46ec10eadf2071a78
yang-xy20 <13760800180@163.com>
2021-07-28 21:41:59 +0800

    update_yxy

--- onpolicy/scripts/train_habitat.sh
+++ onpolicy/scripts/train_habitat.sh
@@ -3,18 +3,15 @@ env="Habitat"
 scenario="pointnav_gibson"
 num_agents=2
 algo="mappo"
-#exp="new_time_penalty_centralized_V_merge_partial_reward_all_rand_rot_pos_center_single_local_ppo4"
 exp="debug"
-seed_max=1
-
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=7 python train/train_habitat.py --scenario_name ${scenario} \
+    CUDA_VISIBLE_DEVICES=0,2 python train/train_habitat.py --scenario_name ${scenario} \
     --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --num_agents ${num_agents} \
     --split "train" --seed 2 --n_training_threads 1 --n_rollout_threads 1 --num_mini_batch 5 \
-    --num_local_steps 15 --max_episode_length 300 --num_env_steps 3000000 --ppo_epoch 4 --gain 0.01 \
+    --num_local_steps 15 --max_episode_length 60 --num_env_steps 3000000 --ppo_epoch 4 --gain 0.01 \
     --lr 2.5e-5 --critic_lr 2.5e-5 --use_maxpool2d \
     --cnn_layers_params '32,3,1,1 64,3,1,1 128,3,1,1 64,3,1,1 32,3,1,1' --hidden_size 256 --log_interval 1 \
     --use_recurrent_policy  --load_slam "../envs/habitat/model/pretrained_models/slam_best.pt" \
