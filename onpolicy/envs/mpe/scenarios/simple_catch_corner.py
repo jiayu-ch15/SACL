1. add configs for fixed valuenorm learning. 2. use variance for prioritized sampling, consider staleness. 3. add new scenario simple_catch_corner.py, which ends the episode when prey is outside or collide.

onpolicy/envs/mpe/scenarios/simple_catch_corner.py
@@ -177,7 +177,7 @@ class Scenario(BaseScenario):
         return main_reward
     
     def done(self, agent, world):
-        if world.world_step >= self.horizon:
+        if self.num_collision >= 1 or self.num_outside >= 1 or world.world_step >= self.horizon:
             return True
         else:
             return False
@@ -196,57 +196,39 @@ class Scenario(BaseScenario):
     def agent_reward(self, agent, world):
         # Agents are negatively rewarded if caught by adversaries
         rew = 0
-        shape = False  # different from openai
-        adversaries = self.adversaries(world)
-        if shape:  # reward can optionally be shaped (increased reward for increased distance from adversary)
-            for adv in adversaries:
-                rew += 0.1 * np.sqrt(np.sum(np.square(agent.state.p_pos - adv.state.p_pos)))
+        good_agents = self.good_agents(world)
+        adv_agents = self.adversaries(world)
+        
         if agent.collide:
-            for a in adversaries:
-                if self.is_collision(a, agent):
-                    rew -= 10
-                    self.num_collision += 1
+            for ag in good_agents:
+                for adv in adv_agents:
+                    if self.is_collision(ag, adv):
+                        rew -= 10
+                        self.num_collision += 1
 
-        # agents are penalized for exiting the screen, so that they can be caught by the adversaries
-        def bound(x):
-            if x < self.corner_max - 0.1:
-                return 0
-            if x < self.corner_max:
-                return (x - (self.corner_max - 0.1)) * 10
-            return min(np.exp(2 * (x - self.corner_max)), 10)
-        for p in range(world.dim_p):
-            x = abs(agent.state.p_pos[p])
-            rew -= bound(x)
-        self.num_outside += np.any(np.abs(agent.state.p_pos) > self.corner_max)
+        # Agents are penalized if outside of boundary.
+        for ag in good_agents:
+            if np.any(np.abs(ag.state.p_pos) > self.corner_max):
+                rew -= 10
+                self.num_outside += 1
 
         return rew
 
     def adversary_reward(self, agent, world):
         # Adversaries are rewarded for collisions with agents
         rew = 0
-        shape = False #different from openai
-        agents = self.good_agents(world)
-        adversaries = self.adversaries(world)
-        if shape:  # reward can optionally be shaped (decreased reward for increased distance from agents)
-            for adv in adversaries:
-                rew -= 0.1 * min([np.sqrt(np.sum(np.square(a.state.p_pos - adv.state.p_pos))) for a in agents])
+        good_agents = self.good_agents(world)
+        adv_agents = self.adversaries(world)
+
         if agent.collide:
-            for ag in agents:
-                for adv in adversaries:
+            for ag in good_agents:
+                for adv in adv_agents:
                     if self.is_collision(ag, adv):
                         rew += 10
 
-        # Make env zero-sum: adversaryies are rewarded if good agents exits the screen.
-        def bound(x):
-            if x < self.corner_max - 0.1:
-                return 0
-            if x < self.corner_max:
-                return (x - (self.corner_max - 0.1)) * 10
-            return min(np.exp(2 * (x - self.corner_max)), 10)
-        for ag in agents:
-            for p in range(world.dim_p):
-                x = abs(ag.state.p_pos[p])
-                rew += bound(x)
+        for ag in good_agents:
+            if np.any(np.abs(ag.state.p_pos) > self.corner_max):
+                rew += 10
 
         return rew
 

onpolicy/runner/competitive/base_runner.py
@@ -46,6 +46,7 @@ class Runner(object):
         self.hidden_size = self.all_args.hidden_size
         self.use_wandb = self.all_args.use_wandb
         self.use_single_network = self.all_args.use_single_network
+        self.use_valuenorm = self.all_args.use_valuenorm
         self.use_render = self.all_args.use_render
         self.recurrent_N = self.all_args.recurrent_N
 
@@ -55,6 +56,8 @@ class Runner(object):
         self.train_blue = (self.training_mode in ["self_play", "blue_br"])
         self.red_model_dir = self.all_args.red_model_dir
         self.blue_model_dir = self.all_args.blue_model_dir
+        self.red_valuenorm_dir = self.all_args.red_valuenorm_dir
+        self.blue_valuenorm_dir = self.all_args.blue_valuenorm_dir
         if self.training_mode == "red_br":
             assert (self.blue_model_dir is not None), ("to train a red br, you should set the value of --blue_model_dir")
         if self.training_mode == "blue_br":
@@ -113,7 +116,6 @@ class Runner(object):
                 self.envs.action_space[0],
                 device=self.device,
             )
-
             self.blue_policy = Policy(
                 self.all_args,
                 self.envs.observation_space[-1],
@@ -121,16 +123,21 @@ class Runner(object):
                 self.envs.action_space[-1],
                 device=self.device,
             )
-            # restore model
-            if self.red_model_dir is not None:
-                self.restore("red")
-            if self.blue_model_dir is not None:
-                self.restore("blue")
 
             # algorithm
             self.red_trainer = TrainAlgo(self.all_args, self.red_policy, device=self.device)
             self.blue_trainer = TrainAlgo(self.all_args, self.blue_policy, device=self.device)
-        
+
+            # restore model
+            if self.red_model_dir is not None:
+                self.restore("red_policy")
+            if self.blue_model_dir is not None:
+                self.restore("blue_policy")
+            if self.use_valuenorm and self.red_valuenorm_dir is not None:
+                self.restore("red_valuenorm")
+            if self.use_valuenorm and self.blue_valuenorm_dir is not None:
+                self.restore("blue_valuenorm")
+
             # buffer
             self.red_buffer = SharedReplayBuffer(
                 self.all_args,
@@ -208,34 +215,47 @@ class Runner(object):
             torch.save(red_policy_actor.state_dict(), str(self.save_dir) + "/red_actor.pt")
             red_policy_critic = self.red_trainer.policy.critic
             torch.save(red_policy_critic.state_dict(), str(self.save_dir) + "/red_critic.pt")
+            if self.use_valuenorm:
+                red_value_normalizer = self.red_trainer.value_normalizer
+                torch.save(red_value_normalizer.state_dict(), str(self.save_dir) + "/red_value_normalizer.pt")
+
             blue_policy_actor = self.blue_trainer.policy.actor
             torch.save(blue_policy_actor.state_dict(), str(self.save_dir) + "/blue_actor.pt")
             blue_policy_critic = self.blue_trainer.policy.critic
             torch.save(blue_policy_critic.state_dict(), str(self.save_dir) + "/blue_critic.pt")
+            if self.use_valuenorm:
+                blue_value_normalizer = self.blue_trainer.value_normalizer
+                torch.save(blue_value_normalizer.state_dict(), str(self.save_dir) + "/blue_value_normalizer.pt")
 
-    def restore(self, side):
-        if side == "red":
+    def restore(self, model):
+        if model == "red_policy":
             if self.use_single_network:
                 red_policy_model_state_dict = torch.load(str(self.red_model_dir) + "/red_model.pt", map_location=self.device)
-                self.red_policy.model.load_state_dict(red_policy_model_state_dict)
+                self.red_trainer.policy.model.load_state_dict(red_policy_model_state_dict)
             else:
                 red_policy_actor_state_dict = torch.load(str(self.red_model_dir) + "/red_actor.pt", map_location=self.device)
-                self.red_policy.actor.load_state_dict(red_policy_actor_state_dict)
-                if not (self.all_args.use_render or self.all_args.use_eval):
-                    red_policy_critic_state_dict = torch.load(str(self.red_model_dir) + "/red_critic.pt", map_location=self.device)
-                    self.red_policy.critic.load_state_dict(red_policy_critic_state_dict)
-        elif side == "blue":
+                self.red_trainer.policy.actor.load_state_dict(red_policy_actor_state_dict)
+                # if not (self.all_args.use_render or self.all_args.use_eval):
+                red_policy_critic_state_dict = torch.load(str(self.red_model_dir) + "/red_critic.pt", map_location=self.device)
+                self.red_trainer.policy.critic.load_state_dict(red_policy_critic_state_dict)
+        elif model == "red_valuenorm":
+            red_value_normalizer_state_dict = torch.load(str(self.red_valuenorm_dir) + "/red_value_normalizer.pt", map_location=self.device)
+            self.red_trainer.value_normalizer.load_state_dict(red_value_normalizer_state_dict)
+        elif model == "blue_policy":
             if self.use_single_network:
                 blue_policy_model_state_dict = torch.load(str(self.blue_model_dir) + "/blue_model.pt", map_location=self.device)
-                self.blue_policy.model.load_state_dict(blue_policy_model_state_dict)
+                self.blue_trainer.policy.model.load_state_dict(blue_policy_model_state_dict)
             else:
                 blue_policy_actor_state_dict = torch.load(str(self.blue_model_dir) + "/blue_actor.pt", map_location=self.device)
-                self.blue_policy.actor.load_state_dict(blue_policy_actor_state_dict)
-                if not (self.all_args.use_render or self.all_args.use_eval):
-                    blue_policy_critic_state_dict = torch.load(str(self.blue_model_dir) + "/blue_critic.pt", map_location=self.device)
-                    self.blue_policy.critic.load_state_dict(blue_policy_critic_state_dict)
+                self.blue_trainer.policy.actor.load_state_dict(blue_policy_actor_state_dict)
+                # if not (self.all_args.use_render or self.all_args.use_eval):
+                blue_policy_critic_state_dict = torch.load(str(self.blue_model_dir) + "/blue_critic.pt", map_location=self.device)
+                self.blue_trainer.policy.critic.load_state_dict(blue_policy_critic_state_dict)
+        elif model == "blue_valuenorm":
+            blue_value_normalizer_state_dict = torch.load(str(self.blue_valuenorm_dir) + "/blue_value_normalizer.pt", map_location=self.device)
+            self.blue_trainer.value_normalizer.load_state_dict(blue_value_normalizer_state_dict)
         else:
-            raise NotImplementedError(f"Side {side} is not supported.")
+            raise NotImplementedError(f"Model {model} is not supported.")
  
     def log_train(self, red_train_infos, blue_train_infos, total_num_steps):
         if self.train_red:

onpolicy/runner/competitive/mpe_curriculum_runner.py
@@ -19,11 +19,13 @@ class MPECurriculumRunner(Runner):
         self.curriculum_buffer = CurriculumBuffer(
             buffer_size=self.all_args.curriculum_buffer_size,
             update_method=self.all_args.update_method,
-            sample_method=self.all_args.sample_method,
+            sample_method="uniform" if self.all_args.sample_metric == "uniform" else "prioritized",
+            max_staleness=self.all_args.max_staleness,
         )
 
         self.no_info = np.ones(self.n_rollout_threads, dtype=bool)
         self.env_infos = dict(
+            initial_dist=np.zeros(self.n_rollout_threads, dtype=int), 
             start_step=np.zeros(self.n_rollout_threads, dtype=int), 
             end_step=np.zeros(self.n_rollout_threads, dtype=int),
             episode_length=np.zeros(self.n_rollout_threads, dtype=int),
@@ -74,8 +76,14 @@ class MPECurriculumRunner(Runner):
                     f"FSP: {int(total_num_steps / (end - start))}."
                 )
                 # training info
+                red_value_mean, red_value_var = self.red_trainer.value_normalizer.running_mean_var()
+                blue_value_mean, blue_value_var = self.blue_trainer.value_normalizer.running_mean_var()
                 red_train_infos["step_reward"] = np.mean(self.red_buffer.rewards)
+                red_train_infos["value_normalizer_mean"] = np.mean(_t2n(red_value_mean))
+                red_train_infos["value_normalizer_var"] = np.mean(_t2n(red_value_var))
                 blue_train_infos["step_reward"] = np.mean(self.blue_buffer.rewards)
+                blue_train_infos["value_normalizer_mean"] = np.mean(_t2n(blue_value_mean))
+                blue_train_infos["value_normalizer_var"] = np.mean(_t2n(blue_value_var))
                 self.log_train(red_train_infos, blue_train_infos, total_num_steps)
                 print(
                     f"adv step reward: {red_train_infos['step_reward']:.2f}, "
@@ -84,6 +92,7 @@ class MPECurriculumRunner(Runner):
                 # env info
                 self.log_env(self.env_infos, total_num_steps)
                 print(
+                    f"initial distance: {np.mean(self.env_infos['initial_dist']):.2f}, "
                     f"start step: {np.mean(self.env_infos['start_step']):.2f}, "
                     f"end step: {np.mean(self.env_infos['end_step']):.2f}, "
                     f"episode length: {np.mean(self.env_infos['episode_length']):.2f}, "
@@ -92,6 +101,7 @@ class MPECurriculumRunner(Runner):
                 )
                 self.no_info = np.ones(self.n_rollout_threads, dtype=bool)
                 self.env_infos = dict(
+                    initial_dist=np.zeros(self.n_rollout_threads, dtype=int), 
                     start_step=np.zeros(self.n_rollout_threads, dtype=int), 
                     end_step=np.zeros(self.n_rollout_threads, dtype=int),
                     episode_length=np.zeros(self.n_rollout_threads, dtype=int),
@@ -198,10 +208,19 @@ class MPECurriculumRunner(Runner):
         )
 
         # curriculum buffer
-        # new_states = [info[0]["state"] for info in infos]
-        # TODO: filter bad states
+        # TODO: better way to filter bad states
+
+        # # only red value
+        # denormalized_values = self.red_trainer.value_normalizer.denormalize(values[:, :self.num_red])
+        
+        # both red and blue value
+        denormalized_red_values = self.red_trainer.value_normalizer.denormalize(values[:, :self.num_red])
+        denormalized_blue_values = self.blue_trainer.value_normalizer.denormalize(values[:, -self.num_blue:])
+        denormalized_values = np.concatenate([denormalized_red_values, -denormalized_blue_values], axis=1)
+        
         new_states = []
-        for info in infos:
+        new_weights = []
+        for info, denomalized_value in zip(infos, denormalized_values):
             state = info[0]["state"]
             if np.any(np.abs(state[0:2]) > self.all_args.corner_max):
                 continue
@@ -214,11 +233,13 @@ class MPECurriculumRunner(Runner):
             if np.any(state[-1] >= self.all_args.horizon):
                 continue
             new_states.append(state)
-        self.curriculum_buffer.insert(new_states)
+            new_weights.append(np.var(denomalized_value))
+        self.curriculum_buffer.insert(new_states, new_weights)
 
         # info dict
         env_dones = np.all(dones, axis=1)
         for idx in np.arange(self.n_rollout_threads)[env_dones * self.no_info]:
+            self.env_infos["initial_dist"][idx] = infos[idx][-1]["initial_dist"]
             self.env_infos["start_step"][idx] = infos[idx][-1]["start_step"]
             self.env_infos["end_step"][idx] = infos[idx][-1]["num_steps"]
             self.env_infos["episode_length"][idx] = infos[idx][-1]["episode_length"]
@@ -334,13 +355,15 @@ class MPECurriculumRunner(Runner):
             adv_step_reward = np.mean(step_rewards[:, :, :self.num_red])
             good_step_reward = np.mean(step_rewards[:, :, -self.num_blue:])
 
+            initial_dist = infos[0][-1]["initial_dist"]
             start_step = infos[0][-1]["start_step"]
             end_step = infos[0][-1]["num_steps"]
             outside_per_step = infos[0][-1]["outside_per_step"]
             collision_per_step = infos[0][-1]["collision_per_step"]
             print(
                 f"episode {episode}: adv step rewards={adv_step_reward:.2f}, good step reward={good_step_reward:.2f}, "
-                f"start step={start_step}, end step={end_step}, outside per step={outside_per_step}, collision per step={collision_per_step}."
+                f"initial_dist={initial_dist}, start step={start_step}, end step={end_step}, "
+                f"outside per step={outside_per_step}, collision per step={collision_per_step}."
             )
 
             # save gif

onpolicy/runner/competitive/mpe_runner.py
@@ -14,6 +14,19 @@ class MPERunner(Runner):
     def __init__(self, config):
         super(MPERunner, self).__init__(config)
 
+        self.no_info = np.ones(self.n_rollout_threads, dtype=bool)
+        self.env_infos = dict(
+            initial_dist=np.zeros(self.n_rollout_threads, dtype=int), 
+            start_step=np.zeros(self.n_rollout_threads, dtype=int), 
+            end_step=np.zeros(self.n_rollout_threads, dtype=int),
+            episode_length=np.zeros(self.n_rollout_threads, dtype=int),
+            outside=np.zeros(self.n_rollout_threads, dtype=bool), 
+            collision=np.zeros(self.n_rollout_threads, dtype=bool),
+            escape=np.zeros(self.n_rollout_threads, dtype=bool),
+            outside_per_step=np.zeros(self.n_rollout_threads, dtype=float), 
+            collision_per_step=np.zeros(self.n_rollout_threads, dtype=float),
+        )
+
     def run(self):
         self.warmup()   
 
@@ -52,30 +65,45 @@ class MPERunner(Runner):
                     f"FSP: {int(total_num_steps / (end - start))}."
                 )
                 # training info
+                red_value_mean, red_value_var = self.red_trainer.value_normalizer.running_mean_var()
+                blue_value_mean, blue_value_var = self.blue_trainer.value_normalizer.running_mean_var()
                 if self.train_red:
                     red_train_infos["step_reward"] = np.mean(self.red_buffer.rewards)
+                    red_train_infos["value_normalizer_mean"] = np.mean(_t2n(red_value_mean))
+                    red_train_infos["value_normalizer_var"] = np.mean(_t2n(red_value_var))
                     print(f"adv step reward: {red_train_infos['step_reward']:.2f}.")
                 if self.train_blue:
                     blue_train_infos["step_reward"] = np.mean(self.blue_buffer.rewards)
+                    blue_train_infos["value_normalizer_mean"] = np.mean(_t2n(blue_value_mean))
+                    blue_train_infos["value_normalizer_var"] = np.mean(_t2n(blue_value_var))
                     print(f"good step reward: {blue_train_infos['step_reward']:.2f}.")
                 self.log_train(red_train_infos, blue_train_infos, total_num_steps)
 
                 # env info
-                start_step = np.array([info[-1]["start_step"] for info in infos])
-                end_step = np.array([info[-1]["num_steps"] for info in infos])
-                outside_per_step = np.array([info[-1]["outside_per_step"] for info in infos])
-                collision_per_step = np.array([info[-1]["collision_per_step"] for info in infos])
+                self.log_env(self.env_infos, total_num_steps)
                 print(
-                    f"start step: {np.mean(start_step):.2f}, "
-                    f"end step: {np.mean(end_step):.2f}, "
-                    f"outside per step: {np.mean(outside_per_step):.2f}, "
-                    f"collision per step: {np.mean(collision_per_step):.2f}.\n"
+                    f"initial distance: {np.mean(self.env_infos['initial_dist']):.2f}, "
+                    f"start step: {np.mean(self.env_infos['start_step']):.2f}, "
+                    f"end step: {np.mean(self.env_infos['end_step']):.2f}, "
+                    f"episode length: {np.mean(self.env_infos['episode_length']):.2f}.\n"
+                    f"outside: {np.mean(self.env_infos['outside']):.2f}, "
+                    f"collision: {np.mean(self.env_infos['collision']):.2f}, "
+                    f"escape: {np.mean(self.env_infos['escape']):.2f}.\n"
+                    f"outside per step: {np.mean(self.env_infos['outside_per_step']):.2f}, "
+                    f"collision per step: {np.mean(self.env_infos['collision_per_step']):.2f}.\n"
                 )
-                env_infos = dict(
-                    start_step=start_step, end_step=end_step,
-                    outside_per_step=outside_per_step, collision_per_step=collision_per_step,
+                self.no_info = np.ones(self.n_rollout_threads, dtype=bool)
+                self.env_infos = dict(
+                    initial_dist=np.zeros(self.n_rollout_threads, dtype=int), 
+                    start_step=np.zeros(self.n_rollout_threads, dtype=int), 
+                    end_step=np.zeros(self.n_rollout_threads, dtype=int),
+                    episode_length=np.zeros(self.n_rollout_threads, dtype=int),
+                    outside=np.zeros(self.n_rollout_threads, dtype=bool), 
+                    collision=np.zeros(self.n_rollout_threads, dtype=bool),
+                    escape=np.zeros(self.n_rollout_threads, dtype=bool),
+                    outside_per_step=np.zeros(self.n_rollout_threads, dtype=float), 
+                    collision_per_step=np.zeros(self.n_rollout_threads, dtype=float),
                 )
-                self.log_env(env_infos, total_num_steps)
 
             # eval
             if self.use_eval and episode % self.eval_interval == 0:
@@ -164,6 +192,20 @@ class MPERunner(Runner):
             masks=masks[:, -self.num_blue:],
         )
 
+        # info dict
+        env_dones = np.all(dones, axis=1)
+        for idx in np.arange(self.n_rollout_threads)[env_dones * self.no_info]:
+            self.env_infos["initial_dist"][idx] = infos[idx][-1]["initial_dist"]
+            self.env_infos["start_step"][idx] = infos[idx][-1]["start_step"]
+            self.env_infos["end_step"][idx] = infos[idx][-1]["num_steps"]
+            self.env_infos["episode_length"][idx] = infos[idx][-1]["episode_length"]
+            self.env_infos["outside"][idx] = (infos[idx][-1]["outside_per_step"] > 0)
+            self.env_infos["collision"][idx] = (infos[idx][-1]["collision_per_step"] > 0)
+            self.env_infos["escape"][idx] = (not self.env_infos["outside"][idx]) and (not self.env_infos["collision"][idx])
+            self.env_infos["outside_per_step"][idx] = infos[idx][-1]["outside_per_step"]
+            self.env_infos["collision_per_step"][idx] = infos[idx][-1]["collision_per_step"]
+            self.no_info[idx] = False
+
     @torch.no_grad()
     def eval(self, total_num_steps):
         eval_adv_step_reward = []
@@ -180,39 +222,55 @@ class MPERunner(Runner):
             eval_good_reward = []
 
             eval_obs = self.eval_envs.reset()
+            eval_share_obs = eval_obs.copy()
             eval_rnn_states = np.zeros((self.n_eval_rollout_threads, self.num_agents, self.recurrent_N, self.hidden_size), dtype=np.float32)
+            eval_rnn_states_critic = np.zeros((self.n_eval_rollout_threads, self.num_agents, self.recurrent_N, self.hidden_size), dtype=np.float32)
             eval_masks = np.ones((self.n_eval_rollout_threads, self.num_agents, 1), dtype=np.float32)
 
             for eval_step in range(self.episode_length):
                 # red action
                 self.red_trainer.prep_rollout()
-                eval_red_actions, eval_red_rnn_states = self.red_trainer.policy.act(
+                eval_red_values, eval_red_actions, _, eval_red_rnn_states, eval_red_rnn_states_critic = self.red_trainer.policy.get_actions(
+                    np.concatenate(eval_share_obs[:, :self.num_red]),
                     np.concatenate(eval_obs[:, :self.num_red]),
                     np.concatenate(eval_rnn_states[:, :self.num_red]),
+                    np.concatenate(eval_rnn_states_critic[:, :self.num_red]),
                     np.concatenate(eval_masks[:, :self.num_red]),
                     deterministic=False,
                 )
+                eval_red_values = np.array(np.split(_t2n(eval_red_values), self.n_eval_rollout_threads))
                 eval_red_actions = np.array(np.split(_t2n(eval_red_actions), self.n_eval_rollout_threads))
                 eval_red_rnn_states = np.array(np.split(_t2n(eval_red_rnn_states), self.n_eval_rollout_threads))
+                eval_red_rnn_states_critic = np.array(np.split(_t2n(eval_red_rnn_states_critic), self.n_eval_rollout_threads))
+                
                 # blue action
                 self.blue_trainer.prep_rollout()
-                eval_blue_actions, eval_blue_rnn_states = self.blue_trainer.policy.act(
+                eval_blue_values, eval_blue_actions, _, eval_blue_rnn_states, eval_blue_rnn_states_critic = self.blue_trainer.policy.get_actions(
+                    np.concatenate(eval_share_obs[:, -self.num_blue:]),
                     np.concatenate(eval_obs[:, -self.num_blue:]),
                     np.concatenate(eval_rnn_states[:, -self.num_blue:]),
+                    np.concatenate(eval_rnn_states_critic[:, -self.num_blue:]),
                     np.concatenate(eval_masks[:, -self.num_blue:]),
                     deterministic=False,
                 )
+                eval_blue_values = np.array(np.split(_t2n(eval_blue_values), self.n_eval_rollout_threads))
                 eval_blue_actions = np.array(np.split(_t2n(eval_blue_actions), self.n_eval_rollout_threads))
                 eval_blue_rnn_states = np.array(np.split(_t2n(eval_blue_rnn_states), self.n_eval_rollout_threads))
+                eval_blue_rnn_states_critic = np.array(np.split(_t2n(eval_blue_rnn_states_critic), self.n_eval_rollout_threads))
+                
                 # concatenate and env action
+                eval_values = np.concatenate([eval_red_values, eval_blue_values], axis=1)
                 eval_actions = np.concatenate([eval_red_actions, eval_blue_actions], axis=1)
                 eval_rnn_states = np.concatenate([eval_red_rnn_states, eval_blue_rnn_states], axis=1)
+                eval_rnn_states_critic = np.concatenate([eval_red_rnn_states_critic, eval_blue_rnn_states_critic], axis=1)
                 eval_actions_env = np.squeeze(np.eye(self.eval_envs.action_space[0].n)[eval_actions], 2)
 
                 # env step
                 eval_obs, eval_rewards, eval_dones, eval_infos = self.eval_envs.step(eval_actions_env)
 
+                eval_share_obs = eval_obs.copy()
                 eval_rnn_states[eval_dones == True] = np.zeros(((eval_dones == True).sum(), self.recurrent_N, self.hidden_size), dtype=np.float32)
+                eval_rnn_states_critic[eval_dones == True] = np.zeros(((eval_dones == True).sum(), self.recurrent_N, self.hidden_size), dtype=np.float32)
                 eval_masks = np.ones((self.n_eval_rollout_threads, self.num_agents, 1), dtype=np.float32)
                 eval_masks[eval_dones == True] = np.zeros(((eval_dones == True).sum(), 1), dtype=np.float32)
 

onpolicy/utils/curriculum_buffer.py
@@ -7,14 +7,17 @@ import torch
 
 class CurriculumBuffer(object):
 
-    def __init__(self, buffer_size, update_method="fps", sample_method="random"):
+    def __init__(self, buffer_size, update_method="fps", sample_method="uniform", max_staleness=5):
         self.eps = 1e-10
         self.buffer_size = buffer_size
         self.update_method = update_method
         self.sample_method = sample_method
+        self.max_staleness = max_staleness
+        # self.max_staleness = np.inf
 
         self._state_buffer = np.zeros((0, 1), dtype=np.float32)
         self._weight_buffer = np.zeros((0, 1), dtype=np.float32)
+        self._staleness_buffer = np.zeros((0, 1), dtype=np.int32)
         self._temp_state_buffer = []
         self._temp_weight_buffer = []
 
@@ -26,7 +29,7 @@ class CurriculumBuffer(object):
         """
         self._temp_state_buffer.extend(states)
         if weights is None:
-            self._temp_weight_buffer.extend([np.zeros(1) for _ in range(len(states))])
+            self._temp_weight_buffer.extend([0 for _ in range(len(states))])
         else:
             self._temp_weight_buffer.extend(weights)
 
@@ -36,27 +39,39 @@ class CurriculumBuffer(object):
         # concatenate to get all states and weight
         all_states = np.array(self._temp_state_buffer)
         all_weights = np.array(self._temp_weight_buffer)
+        all_staleness = np.zeros(len(all_weights), dtype=np.int32)
+
+        # remove states and weight with staleness >= max_staleness
+        self._staleness_buffer += 1
+        remain_idx = (self._staleness_buffer < self.max_staleness)
+        self._state_buffer = self._state_buffer[remain_idx]
+        self._weight_buffer = self._weight_buffer[remain_idx]
+        self._staleness_buffer = self._staleness_buffer[remain_idx]
+
         if self._state_buffer.shape[0] != 0:  # state buffer is not empty
             all_states = np.concatenate([self._state_buffer, all_states], axis=0)
             all_weights = np.concatenate([self._weight_buffer, all_weights], axis=0)
+            all_staleness = np.concatenate([self._staleness_buffer, all_staleness], axis=0)
 
         # update 
         if all_states.shape[0] <= self.buffer_size:
             self._state_buffer = copy.deepcopy(all_states)
             self._weight_buffer = copy.deepcopy(all_weights)
+            self._staleness_buffer = copy.deepcopy(all_staleness)
         else:
             if self.update_method == "random":
                 random_idx = np.random.choice(all_states.shape[0], self.buffer_size, replace=False)
                 self._state_buffer = copy.deepcopy(all_states[random_idx])
                 self._weight_buffer = copy.deepcopy(all_weights[random_idx])
+                self._staleness_buffer = copy.deepcopy(all_staleness[random_idx])
             elif self.update_method == "fps":
                 # farthest point sampling
                 min_states = np.min(all_states, axis=0)
                 max_states = np.max(all_states, axis=0)
                 all_states_normalized = (all_states - min_states) / (max_states - min_states + self.eps)
                 # TODO: mask unnecessary dim
-                consider_dim = np.array([True for _ in range(21)])  # consider everything.
-                # consider_dim = np.array([True for _ in range(20)] + [False])  # do not consider time.
+                # consider_dim = np.array([True for _ in range(21)])  # consider everything.
+                consider_dim = np.array([True for _ in range(20)] + [False])  # w.o. time.
                 # consider_dim = np.array([True for _ in range(16)] + [False for _ in range(4)] + [True])  # do not consider landmarks
                 # consider_dim = np.array([True for _ in range(16)] + [False for _ in range(5)])  # do not consider landmarks and time.
                 all_states_tensor = torch.tensor(all_states_normalized[np.newaxis, :, consider_dim])
@@ -64,6 +79,7 @@ class CurriculumBuffer(object):
                 # update state and weight buffer
                 self._state_buffer = copy.deepcopy(all_states[fps_idx])
                 self._weight_buffer = copy.deepcopy(all_weights[fps_idx])
+                self._staleness_buffer = copy.deepcopy(all_staleness[fps_idx])
             else:
                 raise NotImplementedError(f"Update method {self.update_method} is not supported.")
         
@@ -82,9 +98,14 @@ class CurriculumBuffer(object):
         if self._state_buffer.shape[0] == 0:  # state buffer is empty
             return [None for _ in range(num_samples)]
 
-        if self.sample_method == "random":
+        if self.sample_method == "uniform":
             random_idx = np.random.choice(self._state_buffer.shape[0], num_samples, replace=True)
             initial_states = [self._state_buffer[idx] for idx in random_idx]
+        elif self.sample_method == "prioritized":
+            weights = self._weight_buffer / np.mean(self._weight_buffer)
+            probs = weights ** 5 / np.sum(weights ** 5)
+            sample_idx = np.random.choice(self._state_buffer.shape[0], num_samples, replace=True, p=probs)
+            initial_states = [self._state_buffer[idx] for idx in sample_idx]
         else:
             raise NotImplementedError(f"Sample method {self.update_method} is not supported.")
         