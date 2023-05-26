729b38f52f6796e73e1d84fe0325da1fc03ab003
jiayu-ch15 <jiayu-ch15@jiayu.eva5.nics.info>
2023-05-26 09:10:51 +0000

    update

--- onpolicy/scripts/train_football_br.sh
+++ onpolicy/scripts/train_football_br.sh
@@ -5,7 +5,7 @@ env="Football"
 # scenario="academy_run_pass_and_shoot_with_keeper"
 scenario="academy_3_vs_1_with_keeper"
 algo="mappo"
-exp="rnad_3v1_model1_red@100M"
+exp="sacl_3v1_model3_blue@350M"
 # exp="psro_3v1_model3_blue@100M_again"
 seed=0
 
@@ -39,20 +39,20 @@ ppo_epoch=10 # 5, 10, 15
 num_mini_batch=2 # 2, 4
 
 # red br
-training_mode="red_br"
-blue_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/rnad_3v1_entropy01/wandb/run-20230516_085243-aup94dxv/files"
-red_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/rnad_3v1_entropy01/wandb/run-20230516_085243-aup94dxv/files"
-red_valuenorm_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/rnad_3v1_entropy01/wandb/run-20230516_085243-aup94dxv/files"
+# training_mode="red_br"
+# blue_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/sacl_3v1_500M/wandb/run-20230522_063714-2p5oicm0/files/350M"
+# red_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/sacl_3v1_500M/wandb/run-20230522_063445-md6txb0a/files"
+# red_valuenorm_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/sacl_3v1_500M/wandb/run-20230522_063445-md6txb0a/files"
 
 # blue br
-# training_mode="blue_br"
-# red_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/rnad_3v1_entropy01/wandb/run-20230516_085338-3p7qfz3b/files"
-# blue_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/rnad_3v1_entropy01/wandb/run-20230516_085338-3p7qfz3b/files"
-# blue_valuenorm_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/rnad_3v1_entropy01/wandb/run-20230516_085338-3p7qfz3b/files"
+training_mode="blue_br"
+red_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/sacl_3v1_500M/wandb/run-20230522_063714-2p5oicm0/files/350M"
+blue_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/sacl_3v1_500M/wandb/run-20230522_063445-md6txb0a/files"
+blue_valuenorm_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/Football/academy_3_vs_1_with_keeper/mappo/sacl_3v1_500M/wandb/run-20230522_063445-md6txb0a/files"
 
 echo "n_rollout_threads: ${n_rollout_threads} \t ppo_epoch: ${ppo_epoch} \t num_mini_batch: ${num_mini_batch}"
 
-CUDA_VISIBLE_DEVICES=1 python train/train_football_competitive.py \
+CUDA_VISIBLE_DEVICES=3 python train/train_football_competitive.py \
 --env_name ${env} --scenario_name ${scenario} \
 --algorithm_name ${algo} --experiment_name ${exp} --seed ${seed} \
 --num_agents ${num_agents} --representation ${representation} \
@@ -67,8 +67,8 @@ CUDA_VISIBLE_DEVICES=1 python train/train_football_competitive.py \
 --eval_interval ${eval_interval} --eval_episodes ${eval_episodes} \
 --n_eval_rollout_threads ${n_eval_rollout_threads} \
 --user_name "chenjy" --wandb_name "football" --rewards ${rewards} \
---blue_model_dir ${blue_model_dir} --red_model_dir ${red_model_dir} --red_valuenorm_dir ${red_valuenorm_dir} \
-# --red_model_dir ${red_model_dir} --blue_model_dir ${blue_model_dir} --blue_valuenorm_dir ${blue_valuenorm_dir} \
+--red_model_dir ${red_model_dir} --blue_model_dir ${blue_model_dir} --blue_valuenorm_dir ${blue_valuenorm_dir} \
+# --blue_model_dir ${blue_model_dir} --red_model_dir ${red_model_dir} --red_valuenorm_dir ${red_valuenorm_dir} \
 
 # red br
 # --blue_model_dir ${blue_model_dir} --red_model_dir ${red_model_dir} --red_valuenorm_dir ${red_valuenorm_dir} \
