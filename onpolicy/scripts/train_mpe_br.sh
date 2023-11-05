b8cee1024b7a7e9ab307b89fd9560b2481c63f76
jiayu-ch15 <jiayu-ch15@jiayu.eva5.nics.info>
2023-11-05 17:03:45 +0800

    update

--- onpolicy/scripts/train_mpe_br.sh
+++ onpolicy/scripts/train_mpe_br.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # exp config
-# exp="mappg_sp_model1@5M"
+exp="mappg_sacl_model3@40M"
 # exp="full_fsp_model3@35M-from_pretrained40M"
-exp="debug"
+# exp="debug"
 algo="mappo"
 oppenent_name="mappg"
 seed=0
@@ -19,7 +19,7 @@ num_landmarks=2
 attn_size=32
 # training config
 training_mode="red_br"
-blue_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/MPE/simple_tag_corner/mappg/mappg_sp/wandb/run-20231104_133626-3n27sipz/files/5M"
+blue_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/MPE/simple_tag_corner/mappg/mappg_sacl/wandb/run-20231104_175353-1rzbfifo/files/40M"
 red_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/MPE/simple_tag_corner/mappo/corner_sacl/files/run-20230414_031641-n9dn372p/files"
 red_valuenorm_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/MPE/simple_tag_corner/mappo/corner_sacl/files/run-20230414_031641-n9dn372p/files"
 # red_model_dir="/home/jiayu-ch15/onpolicy/onpolicy/scripts/results/MPE/simple_tag_corner/mappo/least_visited_add_variance_alpha1/wandb/run-20230803_032945-1b7lwd88/files"
@@ -39,7 +39,7 @@ wandb_name="sacl"
 
 echo "exp is ${exp}, env is ${env}, scenario is ${scenario}, algo is ${algo}, seed is ${seed}"
 
-CUDA_VISIBLE_DEVICES=3 python train/train_mpe_competitive.py \
+CUDA_VISIBLE_DEVICES=0 python train/train_mpe_competitive.py \
 --experiment_name ${exp} --algorithm_name ${algo} --seed ${seed} --competitive \
 --env_name ${env} --scenario_name ${scenario} --horizon ${horizon} \
 --corner_min ${corner_min} --corner_max ${corner_max} \
@@ -57,4 +57,4 @@ CUDA_VISIBLE_DEVICES=3 python train/train_mpe_competitive.py \
 --oppenent_name ${oppenent_name} \
 --blue_model_dir ${blue_model_dir} \
 --red_model_dir ${red_model_dir} --red_valuenorm_dir ${red_valuenorm_dir} \
---use_wandb \
+# --use_wandb \
